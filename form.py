f7d779488a9ea821632345314bd27c6b2a03e3d8
gurnoora21 <gurnooraujla10@gmail.com>
2020-02-09 11:23:27 -0700

    added animations to text boxes, and increased size of text input

--- form.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
-
-class Userinput(FlaskForm):
-    question = StringField('question',
-                           validators=[DataRequired(), Length(min=2, max=20)]
-                           )
